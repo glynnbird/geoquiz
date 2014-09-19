ce8ef1d6c0f837a6cbfc939fd73be9da7a81f2b6
Glynn Bird <glynn.bird@gmail.com>
2014-09-19 14:00:35 +0100

    code tidy

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -5,6 +5,7 @@ var started= false;
 var elapsed = 0;
 var timer = null;
 
+// leaflet map options
 var options = {
   zoomControl: false,
   dragging: false,
@@ -12,12 +13,12 @@ var options = {
   scollWheelZoom: false,
   doubleClickZoom: false,
   boxZoom: false,
-  minZoom:zoomLevel,
-  maxZoom:zoomLevel
+  minZoom: zoomLevel,
+  maxZoom: zoomLevel
 }
-var map = L.map('map', options).setView([43.74739, -105], zoomLevel);
 
-var greyStyle = {
+// land styling
+var greenStyle = {
     color: "#666",
     fillColor: "#66bb66",
     fillOpacity: 1.0,
@@ -25,6 +26,7 @@ var greyStyle = {
     opacity: 1
 };
 
+// state styling
 var fancyStyle = {
   color: "#ff8888",
   weight: 3,
@@ -32,18 +34,21 @@ var fancyStyle = {
   fillOpacity: 0.5,
 };
 
-var renderGeoJSON = function(obj,style, label) {
-  var opts = {style: style}
+// generate a map
+var map = L.map('map', options).setView([43.74739, -105], zoomLevel);
+
+// render a GeoJSON object on the map
+var renderGeoJSON = function(obj, style, label) {
+  var opts = {
+    style: style,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(label);
+    }
+  }
   L.geoJson(obj, opts).addTo(map);
 };
 
-$.ajax({url: "/js/world.json",
-        success: function(data) {
-          //console.log(data);
-          renderGeoJSON(data, greyStyle);
-        }
-      });
-      
+// load the state names from Cloudant      
 var fetchStateNames = function() {
   $.ajax({url: "/proxy/usstates/_design/fetch/_view/byName",
           success: function(data) {
@@ -52,49 +57,66 @@ var fetchStateNames = function() {
               states[data.rows[i].id] = data.rows[i].key;
             }
           }
-        }); 
-  
+        });   
 } ;     
       
+// render a JSON object as a table of key/values      
 var jsonToTable = function(d) {
   var table = "<table>";
   for(var i in d) {
-    table += "<tr><th>"+i+"</th><td>"+d[i]+"</td></tr>";
+    table += "<tr><th>" + i + "</th><td>" + d[i] + "</td></tr>";
   }
   table += "</table>";
   return table;
-}      
+};      
       
-      
+// given a state name, load it's GeoJSON from Cloudant and render it      
 var renderState = function(state) {
   $.ajax({url: "/proxy/usstates/" + encodeURIComponent(state),
           success: function(data) {
             data = JSON.parse(data);
-            renderGeoJSON(data, fancyStyle, data.properties.abbreviation);
+            renderGeoJSON(data, fancyStyle, data.properties.name);
             $("#info").html(jsonToTable(data.properties));
           }
         });  
 }
 
+// show the current score e.g 4/50
 var renderScore = function() {
   $('#score').html(mystates.length + " / " + Object.keys(states).length);
 }
 
+// check if state 's' is in our list
 var checkState = function(s) {
+  // first keypress, start the timer
   if(!started) {
     started = true;
     startTimer();
   }
+  
+  // lowercase it
   s = s.toLowerCase();
+  
+  // check of existance in list of states
   var i = mystates.indexOf(s);
   if(i > -1) {
     return;
   } else if (i == -1 ) {
+    
+    // if hasn't already been found 
     if(Object.keys(states).indexOf(s)>-1) {
+      
+      // draw it
       mystates.push(s);
       renderState(s);
+      
+      // clear the input control
       $('#state').val("");
+      
+      // update the score
       renderScore();
+      
+      // check for quiz complete
       if (mystates.length == Object.keys(states).length) {
         stopTimer();
         alert("Quiz complete");
@@ -103,38 +125,62 @@ var checkState = function(s) {
   }
 }
 
+// pad a number with leading zeros
 var pad = function(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
     return s;
 }
 
+// show elapsed time in minutes + seconds
 var renderTimer  = function() {
   var s = elapsed % 60;
   var m = Math.floor(elapsed / 60);
   $('#timer').html(pad(m,2)+":"+pad(s,2));
 }
 
+
+// called when the quiz starts
 var startTimer = function() {
+  
+  // show the score
   renderScore();
   elapsed = 0;
+  
+  // and the timer
   renderTimer();
+  
+  // repeat every second
   timer = setInterval(function() {
     elapsed++;
     renderTimer();
   },1000);
 }
 
+// called at end of the quiz
 var stopTimer = function() {
   clearInterval(timer);
   timer = null;
 };
 
+// called whenever the input control changes state
 var stateChange = function() {
   checkState($('#state').val())
 };
 
-fetchStateNames();
+
+// onload
+$(window).load(function() {
+  // render the world
+  $.ajax({url: "/js/world.json",
+          success: function(data) {
+            renderGeoJSON(data, greenStyle);
+          }
+        });
+
+  fetchStateNames();
+});
+
       
       
 
